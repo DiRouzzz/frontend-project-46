1c44e6385db92a5477b04633e8091ef22f637459
root <root@DESKTOP-FII83V8>
2024-10-25 19:36:20 +0400

    update index.js & package.json & README.md

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
-import fs from 'node:fs'
-import path from 'node:path'
-import { cwd } from 'node:process'
+import fs from 'node:fs';
+import path from 'node:path';
+import { cwd } from 'node:process';
+import _ from 'lodash';
 
-// const a = fs.readFileSync('__fixtures__/file1.json', { encoding: 'utf-8' })
-// console.log(a)
-// const b = path.resolve('file2.json')
-// console.log(b)
-
-console.log(`dir: ${cwd()}`)
 function genDiff(filepath1, filepath2) {
 	const file1 = JSON.parse(
 		fs.readFileSync(path.resolve(filepath1 || `${cwd()}/${filepath1}`), {
@@ -20,9 +15,35 @@ function genDiff(filepath1, filepath2) {
 			encoding: 'utf-8',
 		})
 	)
-	return { file1, file2 }
+
+	const f1 = Object.entries(file1);
+	const f2 = Object.entries(file2);
+	const files = _.union(f1, f2).sort();
+
+	let result = '';
+
+	for (const [key, value] of files) {
+		if (!Object.hasOwn(file1, key)) {
+			result += `+ ${key}: ${value}`;
+		} else if (!Object.hasOwn(file2, key)) {
+			result += `- ${key}: ${value}\n`;
+		} else if (
+			Object.hasOwn(file1, key) === Object.hasOwn(file2, key) &&
+			file1[key] !== file2[key]
+		) {
+			if (
+				!result.includes(`- ${key}: ${file1[key]}\n`) &&
+				!result.includes(`+ ${key}: ${file2[key]}\n`)
+			) {
+				result += `- ${key}: ${file1[key]}\n`;
+				result += `+ ${key}: ${file2[key]}\n`;
+			}
+		} else if (!result.includes(`  ${key}: ${value}, \n`)) {
+			result += `  ${key}: ${value}, \n`;
+		}
+	}
+
+	return result;
 }
 
-export default genDiff
-
-// console.log(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json'))
+export default genDiff;
