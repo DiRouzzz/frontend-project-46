ef24d14a6cfd3c9fc76316637df0e760989d94c7
root <root@DESKTOP-FII83V8>
2024-10-28 17:05:35 +0400

    update gendiff.js & package.json & index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,51 @@
 #!/usr/bin/env node
-import fs from 'node:fs';
+/* eslint-disable no-tabs */
 import path from 'node:path';
-import { cwd } from 'node:process';
 import _ from 'lodash';
+import jsonParse from './parsers/jsonParse.js';
+import yamlParse from './parsers/parseYaml.js';
 
 function genDiff(filepath1, filepath2) {
-	const file1 = JSON.parse(
-		fs.readFileSync(path.resolve(filepath1 || `${cwd()}/${filepath1}`), {
-			encoding: 'utf-8',
-		})
-	)
-	const file2 = JSON.parse(
-		fs.readFileSync(path.resolve(filepath2 || `${cwd()}/${filepath2}`), {
-			encoding: 'utf-8',
-		})
-	)
+  let file1;
+  let file2;
+  if ((path.extname(filepath1) && path.extname(filepath2)) === '.json') {
+    file1 = jsonParse(filepath1);
+    file2 = jsonParse(filepath2);
+  } else if ((path.extname(filepath1) && path.extname(filepath2)) === '.yaml') {
+    file1 = yamlParse(filepath1);
+    file2 = yamlParse(filepath2);
+  }
 
-	const f1 = Object.entries(file1);
-	const f2 = Object.entries(file2);
-	const files = _.union(f1, f2).sort();
+  const f1 = Object.entries(file1);
+  const f2 = Object.entries(file2);
+  const files = _.union(f1, f2).sort();
 
-	let result = '';
+  let result = '';
 
-	for (const [key, value] of files) {
-		if (!Object.hasOwn(file1, key)) {
-			result += `+ ${key}: ${value}`;
-		} else if (!Object.hasOwn(file2, key)) {
-			result += `- ${key}: ${value}\n`;
-		} else if (
-			Object.hasOwn(file1, key) === Object.hasOwn(file2, key) &&
-			file1[key] !== file2[key]
-		) {
-			if (
-				!result.includes(`- ${key}: ${file1[key]}\n`) &&
-				!result.includes(`+ ${key}: ${file2[key]}\n`)
-			) {
-				result += `- ${key}: ${file1[key]}\n`;
-				result += `+ ${key}: ${file2[key]}\n`;
-			}
-		} else if (!result.includes(`  ${key}: ${value}, \n`)) {
-			result += `  ${key}: ${value}, \n`;
-		}
-	}
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [key, value] of files) {
+    if (!Object.hasOwn(file1, key)) {
+      result += `  + ${key}: ${value}`;
+    } else if (!Object.hasOwn(file2, key)) {
+      result += `  - ${key}: ${value}\n`;
+    } else if (
+      Object.hasOwn(file1, key) === Object.hasOwn(file2, key)
+			// eslint-disable-next-line no-tabs
+			&& file1[key] !== file2[key]
+    ) {
+      if (
+        !result.includes(`  - ${key}: ${file1[key]}\n`)
+				&& !result.includes(`  + ${key}: ${file2[key]}\n`)
+      ) {
+        result += `  - ${key}: ${file1[key]}\n`;
+        result += `  + ${key}: ${file2[key]}\n`;
+      }
+    } else if (!result.includes(`    ${key}: ${value}\n`)) {
+      result += `    ${key}: ${value}\n`;
+    }
+  }
 
-	return `{\n${result}\n}`;
+  return `{\n${result}\n}`;
 }
 
 export default genDiff;
