eebd41da1d39e7db6cb163b042f4a888905c2dc4
root <root@DESKTOP-FII83V8>
2024-10-31 13:39:35 +0400

    update gendiff.js & index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import genDiff from '../src/index.js';
+import parseFile from '../src/parsers/parseFile.js';
 
 program
   .version('1.0.0')
@@ -8,6 +9,6 @@ program
   .arguments('filepath1 filepath2')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    console.log(genDiff(parseFile(filepath1), parseFile(filepath2)));
   })
   .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,38 +1,43 @@
 #!/usr/bin/env node
 import _ from 'lodash';
-import parseFile from './parsers/parseFile.js';
 
-function genDiff(filepath1, filepath2) {
-  const file1 = parseFile(filepath1);
-  const file2 = parseFile(filepath2);
+function genDiff(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
-  const f1 = Object.entries(file1);
-  const f2 = Object.entries(file2);
-  const files = _.union(f1, f2).sort();
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  let result = '';
-
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [key, value] of files) {
-    if (!Object.hasOwn(file1, key)) {
-      result += `  + ${key}: ${value}`;
-    } else if (!Object.hasOwn(file2, key)) {
-      result += `  - ${key}: ${value}\n`;
-    } else if (
-      Object.hasOwn(file1, key) === Object.hasOwn(file2, key) && file1[key] !== file2[key]
-    ) {
-      if (
-        !result.includes(`  - ${key}: ${file1[key]}\n`) && !result.includes(`  + ${key}: ${file2[key]}\n`)
-      ) {
-        result += `  - ${key}: ${file1[key]}\n`;
-        result += `  + ${key}: ${file2[key]}\n`;
-      }
-    } else if (!result.includes(`    ${key}: ${value}\n`)) {
-      result += `    ${key}: ${value}\n`;
+  const result = sortedKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return {
+        name: key, type: 'added', value: obj2[key],
+      };
     }
-  }
 
-  return `{\n${result}\n}`;
+    if (!_.has(obj2, key)) {
+      return {
+        name: key, type: 'removed', value: obj1[key],
+      };
+    }
+
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return {
+        name: key, type: 'nested', children: genDiff(obj1[key], obj2[key]),
+      };
+    }
+
+    if (obj1[key] !== obj2[key]) {
+      return {
+        name: key, type: 'changed', oldValue: obj1[key], newValue: obj2[key],
+      };
+    }
+
+    return {
+      name: key, type: 'unchanged', value: obj1[key],
+    };
+  });
+
+  return result;
 }
 
 export default genDiff;
